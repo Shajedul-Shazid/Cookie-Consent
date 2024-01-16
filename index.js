2e7c0b4ccb41eda70c49ff993255bca0dd9caddc
Shajedul-Shazid <programmershajid@gmail.com>
2024-01-16 19:18:12 +0600

    add preventDefault() functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const modalEl = document.getElementById("modal")
 const modalCloseBtn = document.getElementById("modal-close-btn")
+const loginForm = document.getElementById("login-form")
 
 setTimeout(function(){
     modalEl.style.display = 'block'
@@ -8,4 +9,9 @@ setTimeout(function(){
 modalCloseBtn.addEventListener('click', function() {
     modalEl.style.display = 'none'
 })
+
+loginForm.addEventListener("submit", function(e){
+    e.preventDefault()
+})
+
 
